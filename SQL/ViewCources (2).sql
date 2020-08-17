20ddd6f25f1ca56696861ecac93bfbb01da82973
leahherman <leah.hermann@gmail.com>
2020-08-18 00:47:30 +0300

    Adding SQL Views

---
+++ SQL/ViewCources (2).sql
@@ -0,0 +1,19 @@
+
+
+
+
+ALTER  VIEW courses_data_view  AS
+
+
+select  DISTINCT CO.CourseName , D.DepartmentName , TEACH.FirstName +' ' +TEACH.LastName   as  TeachersName
+
+ ,  count(cl.StudentId) OVER (ORDER by CO.CourseId)  as numStd  
+
+ from  COLLEGE.dbo.Courses$  as co
+ JOIN Classrooms$  AS CL
+ ON CL.CourseId = CO.CourseId
+ JOIN Departments$ AS D
+ ON D.DepartmentId = CO.DepartmentID  
+ JOIN Teachers$  AS TEACH
+ ON  TEACH.TeacherId = CO.TeacherId
+
